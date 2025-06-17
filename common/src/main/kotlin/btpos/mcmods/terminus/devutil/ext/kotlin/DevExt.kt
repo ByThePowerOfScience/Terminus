a7a40db8e504634b743c43f34cab4619871b7756
ByThePowerOfScience <16433721+ByThePowerOfScience@users.noreply.github.com>
2025-06-17 14:29:14 -0400

    DevExt#ifNull: inline it

--- common/src/main/kotlin/btpos/mcmods/terminus/devutil/ext/kotlin/DevExt.kt
+++ common/src/main/kotlin/btpos/mcmods/terminus/devutil/ext/kotlin/DevExt.kt
@@ -1,6 +1,6 @@
 package btpos.mcmods.terminus.devutil.ext.kotlin
 
-fun <T> T?.ifNull(action: () -> Unit): T? {
+inline fun <T> T?.ifNull(action: () -> Unit): T? {
 	if (this == null)
 		action()
 	return this
